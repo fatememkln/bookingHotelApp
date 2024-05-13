142162611ea55b47956dfe969173a89edca5d742
fatememkln <fateme.mkln@gmail.com>
2024-05-13 19:24:19 +0330

    picture url

--- src/components/LocationList/LocationList.jsx
+++ src/components/LocationList/LocationList.jsx
@@ -17,7 +17,7 @@ function LocationList() {
         {data.map((item) => {
           return (
             <div className="locationItem" key={item.id}>
-              <img src={item.picture_url.url} alt={item.name} />
+              <img src={item.thumbnail_url} alt={item.name} />
               <div className="locationItemDesc">
                 <p className="location">{item.smart_location}</p>
                 <p className="name">{item.smart_name}</p>
